3ed8599eac68b4d8283aa8d32a49c4b434dc758e
AvneeshPillai <102538628+AvneeshPillai@users.noreply.github.com>
2022-06-21 01:33:32 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,77 @@
+'''import math as m
+l=int(input("Enter the length of prism: "))
+b=int(input("Enter the breadth of prism: "))
+h=int(input("Enter the height of prism: "))
+u=input("Enter the unit of prism: ")
+vol=l*b*h
+print("The volume of prism is ",vol,"cubic ",u)
+
+d=m.sqrt(l**2+b**2+h**2)
+print("The diagonal length of the prism: ",d)'''
+a=5
+print(a)
+
+
+var="Python programming"
+s="exp\tand\ttab"
+s2="         python        "
+s3='helloworld'
+print(var[15])
+print(len(var))
+print(var[0:6])
+print(var[7:14])
+print(var[2:4]+var[15:18])
+print(var.upper())
+var_2="is interesting"
+print(var +" "+ var_2)
+print(s.capitalize())
+print(var.casefold())
+print(var.center(20,'#'))
+print(var.count('m'))
+print(s.expandtabs(8))
+print(var.find('m'))
+print(var.format())
+print(var.zfill(20))
+print('welcome to',s2.strip(),'programming')
+print(var.swapcase())
+print(var.rpartition('pro'))
+print(s.replace('\t','-'))
+print(s3.isalpha())
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+
+
+
+
+
+
+
+
+
